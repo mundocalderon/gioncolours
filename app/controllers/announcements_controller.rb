23c5a29a5ed68c7f603ebb562d7255ea073930f0
mundocalderon <mundocalderon@gmail.com>
2014-11-07 14:27:48 -0800

    made a few changes to reorder announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -5,7 +5,7 @@ class AnnouncementsController < ApplicationController
   # GET /announcements
   # GET /announcements.json
   def index
-    @announcements = Announcement.all.reverse
+    @announcements = Announcement.all.order('post_date desc')
   end
 
   # GET /announcements/1
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def admin_home
-    @announcements = Announcement.all.order('id desc').limit(6)
+    @announcements = Announcement.all.order('post_date desc').limit(6)
     @gigs = Gig.where("date >= ?",Date.today).sort_by &:date
     @videos = Video.all
   end
@@ -11,13 +11,13 @@ class StaticPagesController < ApplicationController
   end
   
   def joel
-    @announcements = Announcement.joins(:announcement_types).where(announcement_types: {id: 1}).order('id desc').limit(6)
+    @announcements = Announcement.joins(:announcement_types).where(announcement_types: {id: 1}).order('post_date desc').limit(6)
     @gigs = Gig.where("date >= ?",Date.today).sort_by &:date
     @videos = Video.joins(:video_types).where(video_types: {id: 1})
   end
   
   def tpc
-    @announcements = Announcement.joins(:announcement_types).where(announcement_types: {id: 2}).order('id desc').limit(6)
+    @announcements = Announcement.joins(:announcement_types).where(announcement_types: {id: 2}).order('post_date desc').limit(6)
     @gigs = Gig.where("date >= ?",Date.today).sort_by &:date
     @videos = Video.joins(:video_types).where(video_types: {id: 2})
   end
